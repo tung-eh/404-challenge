916ad2ada743cfa923ea280b57fc62517e6831ed
Tung Teng <thetungctn@gmail.com>
2021-01-13 03:53:02 +0700

    404 page

--- src/App.js
+++ src/App.js
@@ -1,9 +1,30 @@
+import React from 'react'
+import ScarecrowImg from './Scarecrow.png'
+
 function App() {
   return (
-    <div className="grid place-items-center w-screen h-screen">
-      Content here
+    <div className="grid grid-rows-layout items-center w-screen min-h-screen px-6 py-4 md:px-14 md:py-6">
+      <div className="uppercase font-logo font-bold text-lg">404 Not Found</div>
+      <div className="font-mono grid gap-10 md:gap-20 grid-cols-one-col md:grid-cols-two-cols my-20">
+        <img src={ScarecrowImg} alt="scarecrow" />
+        <div>
+          <h1 className="font-bold text-5xl md:text-6xl max-w-xl leading-snug md:leading-normal mb-7">
+            I have bad news for you
+          </h1>
+          <p className="font-normal max-w-xs mb-12">
+            The page you are looking for might be removed or is temporarily
+            unavailable
+          </p>
+          <button className="px-8 py-4 bg-gray-800 text-white text-xs uppercase">
+            Back to Homepage
+          </button>
+        </div>
+      </div>
+      <div className="font-sans text-xs text-center">
+        Tung Teng @ DevChallenges.io
+      </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,26 @@
+const colors = require('tailwindcss/colors')
+
 module.exports = {
   purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      gridTemplateRows: {
+        layout: 'auto 1fr auto',
+      },
+      gridTemplateColumns: {
+        'one-col': '1fr',
+        'two-cols': '2fr 3fr',
+      },
+      colors: {
+        gray: colors.trueGray,
+      },
+    },
+    fontFamily: {
+      sans: ['Montserrat', 'sans-serif'],
+      mono: ['"Space Mono"', 'mono'],
+      logo: ['Inconsolata', 'sans-serif'],
+    },
   },
   variants: {
     extend: {},
